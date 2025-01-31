af8f720e0476f1aa92a698ad6f0f2dd2466fc802
Brodzman <thbrodz@gmail.com>
2025-01-31 19:05:56 +1100

    Add a battlefield

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from classes import *
 from constants import *
+
 import random
+import os
 
 def main():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    player, weapon = game_start()
+    enemies = level_1()
+    init_battlefield(player, enemies)
+
+def init_battlefield(player, enemies):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print('A challenge appears!\n---------------------')
+    for enemy in enemies:
+        print(enemy.name, end='  ')
+    print(f'\n\n{player.name}')
+
+def game_start():
     player = create_player(input('Enter name: '), input('Enter role\n 1. Barbarian\n 2. Mage\n 3. Rogue\n'))
     weapon = starting_weapon(player)
-    enemy = enemy_spawner()
-    print(enemy.health)
-    player.deal_damage(player.weapon_damage(player, weapon), enemy)
-    print(enemy.health)
+    return player, weapon
 
 def create_player(name, role):
     if role == '1':
@@ -40,6 +52,13 @@ def enemy_spawner():
     slime.health = 50
     return slime
 
+def level_1():
+    enemy_list = []
+    enemy1 = enemy_spawner()
+    enemy2 = enemy_spawner()
+    enemy_list.append(enemy1)
+    enemy_list.append(enemy2)
+    return enemy_list
 
 
 if __name__ == "__main__":
