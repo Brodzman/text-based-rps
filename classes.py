c1b395ba286202aadd914e53eaaaac14c6706cd6
Brodzman <thbrodz@gmail.com>
2025-01-31 17:26:21 +1100

    Add classes and take damage method

---
+++ classes.py
@@ -0,0 +1,16 @@
+class Character:
+    def __init__(self, name, health, damage):
+        self.name = name
+        self.health = int(health)
+        self.damage = int(damage)
+
+    def take_damage(self, incoming_dmg):
+        self.health -= incoming_dmg
+        
+class Player(Character):
+    def __init__(self, name, health, damage):
+        super().__init__(name, health, damage)
+
+class Enemy(Character):
+    def __init__(self, name, health, damage):
+        super().__init__(name, health, damage)
---
+++ main.py
@@ -0,0 +1,17 @@
+from classes import *
+
+def main ():
+    steven = Player
+    steven.name = 'steven'
+    steven.damage = 2
+    steven.health = 10
+
+    slime = Enemy
+    slime.name = 'slime'
+    slime.damage = 2
+    slime.health = 10
+
+
+
+if __name__ == "__main__":
+    main()
