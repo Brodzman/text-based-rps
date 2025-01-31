8301b4f0d03ced4416073faeb74359d1f915d49a
Brodzman <thbrodz@gmail.com>
2025-01-31 18:36:44 +1100

    Fix up weapon classes and damaging function and add damage multiplers

--- classes.py
+++ classes.py
@@ -8,16 +8,21 @@ class Character:
         self.damage = int(damage)
         self.speed = int(speed)
 
-    def take_damage(self, incoming_dmg):
-        self.health -= incoming_dmg
+    def deal_damage(damage, target):
+        target.health -= damage
+
+    def weapon_damage(self, weapon):
+        total_damage = self.damage * weapon.multiplyer
+        return total_damage
         
 class Player(Character):
     def __init__(self, name, health, damage, speed):
         super().__init__(name, health, damage, speed)
 
-    def set_stats(self, health, damage, speed):
+    def set_stats(self):
         pass
 
+# Each role declaration
 class Barbarian(Player):
     def __init__(self, name, health, damage, speed):
         super().__init__(name, health, damage, speed)
@@ -26,6 +31,24 @@ class Barbarian(Player):
         self.health = BARBARIAN_START_HEALTH
         self.damage = BARBARIAN_START_DAMAGE
         self.speed = BARBARIAN_START_SPEED
+
+class Mage(Player):
+    def __init__(self, name, health, damage, speed):
+        super().__init__(name, health, damage, speed)
+
+    def set_stats(self):
+        self.health = MAGE_START_HEALTH
+        self.damage = MAGE_START_DAMAGE
+        self.speed = MAGE_START_SPEED
+
+class Rogue(Player):
+    def __init__(self, name, health, damage, speed):
+        super().__init__(name, health, damage, speed)
+
+    def set_stats(self):
+        self.health = ROGUE_START_HEALTH
+        self.damage = ROGUE_START_DAMAGE
+        self.speed = ROGUE_START_SPEED
 
 
 class Enemy(Character):
@@ -37,4 +60,27 @@ class Weapon:
         self.name = name
         self.multiplyer = multiplyer
         self.type = type
+    
+    def set_attributes():
+        pass
 
+class Axe(Weapon):
+    def __init__(self, name, multiplyer, type):
+        super().__init__(name, multiplyer, type)
+
+    def set_attributes(self):
+        self.multiplyer = 2
+
+class Staff(Weapon):
+    def __init__(self, name, multiplyer, type):
+        super().__init__(name, multiplyer, type)
+
+    def set_attributes(self):
+        self.multiplyer = 1.5
+
+class Daggers(Weapon):
+    def __init__(self, name, multiplyer, type):
+        super().__init__(name, multiplyer, type)
+
+    def set_attributes(self):
+        self.multiplyer = 1
--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import random
 
 def main():
     player = create_player(input('Enter name: '), input('Enter role\n 1. Barbarian\n 2. Mage\n 3. Rogue\n'))
+    weapon = starting_weapon(player)
     enemy = enemy_spawner()
-    print(player.damage)
-    print(player.health)
+    print(enemy.health)
+    player.deal_damage(player.weapon_damage(player, weapon), enemy)
+    print(enemy.health)
 
 def create_player(name, role):
     if role == '1':
@@ -16,16 +18,26 @@ def create_player(name, role):
     elif role == '3':
         player = Rogue
     else:
-        create_player(name, input('Invalid input'))
+        create_player(name, input('Invalid input')) # Fix bug here
     player.name = name
     player.set_stats(player)
     return player
 
+def starting_weapon(player):
+    if player == Barbarian:
+        weapon = Axe
+    elif player == Mage:
+        weapon = Staff
+    elif player == Rogue:
+        weapon = Daggers    
+    weapon.set_attributes(weapon)
+    return weapon
+
 def enemy_spawner():
     slime = Enemy
     slime.name = 'slime'
-    slime.damage = random.randint(1, 50)
-    slime.health = random.randint(50,100)
+    slime.damage = 50
+    slime.health = 50
     return slime
 
 
