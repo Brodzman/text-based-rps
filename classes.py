d1424c6049508e79723a6d1be2368eda2b3c27c4
Brodzman <thbrodz@gmail.com>
2025-01-31 18:13:19 +1100

    Add role selection and stats

--- classes.py
+++ classes.py
@@ -1,19 +1,36 @@
+from constants import *
+
+# Base Character class
 class Character:
-    def __init__(self, name, health, damage):
+    def __init__(self, name, health, damage, speed):
         self.name = name
         self.health = int(health)
         self.damage = int(damage)
+        self.speed = int(speed)
 
     def take_damage(self, incoming_dmg):
         self.health -= incoming_dmg
         
 class Player(Character):
-    def __init__(self, name, health, damage):
-        super().__init__(name, health, damage)
+    def __init__(self, name, health, damage, speed):
+        super().__init__(name, health, damage, speed)
+
+    def set_stats(self, health, damage, speed):
+        pass
+
+class Barbarian(Player):
+    def __init__(self, name, health, damage, speed):
+        super().__init__(name, health, damage, speed)
+
+    def set_stats(self):
+        self.health = BARBARIAN_START_HEALTH
+        self.damage = BARBARIAN_START_DAMAGE
+        self.speed = BARBARIAN_START_SPEED
+
 
 class Enemy(Character):
-    def __init__(self, name, health, damage):
-        super().__init__(name, health, damage)
+    def __init__(self, name, health, damage, speed):
+        super().__init__(name, health, damage, speed)
 
 class Weapon:
     def __init__(self, name, multiplyer, type):
--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from classes import *
+from constants import *
 import random
 
 def main():
-    player = create_player(input('Enter name: '))
+    player = create_player(input('Enter name: '), input('Enter role\n 1. Barbarian\n 2. Mage\n 3. Rogue\n'))
     enemy = enemy_spawner()
-    print(enemy.damage)
-    print(player.name)
+    print(player.damage)
+    print(player.health)
 
-def create_player(name):
-    player = Player
+def create_player(name, role):
+    if role == '1':
+        player = Barbarian
+    elif role == '2':
+        player = Mage
+    elif role == '3':
+        player = Rogue
+    else:
+        create_player(name, input('Invalid input'))
     player.name = name
-    player.damage = 25
-    player.health = 100
+    player.set_stats(player)
     return player
 
 def enemy_spawner():
